c1683597693ee7413f4b70d6194f843b685fe47f
Nicola <nyc.zyp@gmail.com>
2023-09-20 20:25:55 +0100

    1.5 - Introducing Unity, 1.6 - Introducing Prefabs, 1.7 - Your First Script

---
+++ Assets/PrintToConsole.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintToConsole : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello, I have no doors or windows, who on earth designed me?");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
